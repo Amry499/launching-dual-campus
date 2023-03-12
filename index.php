cf7e48a9ebe8dfdfc3deee2310bbb9b9192a2a60
Amry499 <70644743+Amry499@users.noreply.github.com>
2023-03-12 23:27:18 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>LAUNCHING</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+
+
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos1.css" rel="stylesheet" />
+    <link
+      href="assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/4.13.0/bodymovin.min.js"/> -->
+
+    <link href="assets/js/index.js" />
+    <link href="assets/js/lottie.js" />
+    
+
+    <!-- =======================================================
+  * Template Name: eStartup
+  * Updated: Mar 09 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  </head>
+<style>
+  .bg{
+        background-image: url("assets/img/bg.jpg");
+        height: 100vh;
+      }
+
+      /* Height for devices larger than 576px */
+      @media (min-width: 992px) {
+        .bg {
+          margin-top: -58.59px;
+        }
+      }
+  #bm{
+    width: 25px;
+    height: auto;
+  }
+</style>
+<style>
+
+video::-webkit-media-controls {
+  display: none !important;
+
+}
+.animated
+{
+  animation: up-down 2s ease-in-out infinite alternate-reverse both;
+}
+
+</style>
+<div id="bm">
+
+</div>
+  <body class="bg">
+   
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="hero">
+      <div class="hero-container" data-aos="fade-in">
+        <br>
+        
+        <video autoplay muted playsinline width="100%" style="max-width:50%;" controls="false"  alt="Hero Imgs"
+        data-aos="zoom-out"
+        data-aos-delay="100">
+          <source src="FINAL3.mp4" type="video/mp4">
+        
+        </video>
+        <!-- <img
+          src="assets/img/kn-img.png"
+          alt="Hero Imgs"
+          data-aos="zoom-out"
+          data-aos-delay="100"
+        /> -->
+        <br>
+        <h1  data-aos="zoom-out"
+          data-aos-delay="250" >GOOD MORNING</h1>
+        <h1 data-aos="zoom-out"
+          data-aos-delay="300" >YB DATO' SERI MOHAMED KHALED NORDIN</h1>
+        <h2 class="font" data-aos="zoom-out"
+          data-aos-delay="300"  >Minister of Higher Education of Malaysia</h2>
+        <br>
+        <h1 style="font-size:20px; " data-aos="zoom-out"
+          data-aos-delay="200">Please press MSU logo to officiate the</h1>
+        <h1 style="font-size:20px;" data-aos="zoom-out"
+          data-aos-delay="200">Launching Ceremony</h1>
+         <a href="index1.php"><img class="img-fluid animated"
+          src="logo1.png"
+          alt="Hero Imgs"
+          data-aos="zoom-out"
+          data-aos-delay="150" width="100%" style="max-width:50%;"
+          
+        /></a>
+
+        <div
+           
+            data-aos="fade-left"
+          data-aos-delay="150" width="100%" style="max-width:50%;"
+          >
+            <img
+              src="logo.png"
+              class="img-fluid animated"
+              alt=""
+            />
+          </div>
+        <!-- <a href="index1.html" class="btn-get-started scrollto">Proceed</a> -->
+        <br><br>
+        <!-- <div class="btns">
+          <a href="#"><i class="fa fa-apple fa-3x"></i> App Store</a>
+          <a href="#"><i class="fa fa-play fa-3x"></i> Google Play</a>
+          <a href="#"><i class="fa fa-windows fa-3x"></i> windows</a>
+        </div> -->
+      </div>
+    </section>
+    <!-- End Hero Section -->
+
+    <!-- ======= Footer ======= -->
+   
+
+          <!-- <div class="col-sm-6 col-md-3 col-lg-2">
+            <div class="list-menu">
+              <h4>Abou Us</h4>
+
+              <ul class="list-unstyled">
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Features item</a></li>
+                <li><a href="#">Live streaming</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3 col-lg-2">
+            <div class="list-menu">
+              <h4>Abou Us</h4>
+
+              <ul class="list-unstyled">
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Features item</a></li>
+                <li><a href="#">Live streaming</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3 col-lg-2">
+            <div class="list-menu">
+              <h4>Support</h4>
+
+              <ul class="list-unstyled">
+                <li><a href="#">faq</a></li>
+                <li><a href="#">Editor help</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-md-3 col-lg-2">
+            <div class="list-menu">
+              <h4>Abou Us</h4>
+
+              <ul class="list-unstyled">
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Features item</a></li>
+                <li><a href="#">Live streaming</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div> -->
+
+
+    </footer>
+    <!-- End  Footer -->
+
+    <a
+      href="#"
+      class="back-to-top d-flex align-items-center justify-content-center"
+      ><i class="bi bi-arrow-up-short"></i
+    ></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
---
+++ index1.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>PROGRESS</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link
+      href="assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link
+      href="assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/prog.css" />
+    <link href="assets/js/index.js" />
+
+    
+
+    <!-- =======================================================
+  * Template Name: eStartup
+  * Updated: Mar 09 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  </head>
+<style>
+  .bg{
+        background-image: url("assets/img/bg.jpg");
+        height: 100vh;
+      }
+
+      /* Height for devices larger than 576px */
+      @media (min-width: 992px) {
+        .bg {
+          margin-top: -58.59px;
+        }
+      }
+  #bm{
+    width: 25px;
+    height: auto;
+  }
+</style>
+<!-- <style>video::-webkit-media-controls {
+  display: none !important;
+}
+</style> -->
+<div id="bm">
+
+</div>
+  <body class="bg">
+   
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+      <div class="hero-container" data-aos="fade-in">
+        
+      
+        <div class="loading">
+          <div class="text" id="text">Loading</div>
+          <label class="percent">100%</label>
+          <div class="progress-bar">
+            <div class="progress"></div>
+          </div>
+        </div>
+        <!-- <div class="btns">
+          <a href="#"><i class="fa fa-apple fa-3x"></i> App Store</a>
+          <a href="#"><i class="fa fa-play fa-3x"></i> Google Play</a>
+          <a href="#"><i class="fa fa-windows fa-3x"></i> windows</a>
+        </div> -->
+      </div>
+    </section>
+    <!-- End Hero Section -->
+
+    <!-- ======= Footer ======= -->
+    
+    <!-- End  Footer -->
+
+    <a
+      href="#"
+      class="back-to-top d-flex align-items-center justify-content-center"
+      ><i class="bi bi-arrow-up-short"></i
+    ></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/index.js"></script>
+  </body>
+</html>
